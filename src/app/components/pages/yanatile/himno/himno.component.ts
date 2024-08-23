3246284ad51231eb31993945ed5f55d2cf0b4cf9
Ronald Huahuatico Soria <69460625+Romehe369@users.noreply.github.com>
2024-08-23 13:25:47 -0500

    create components
    nothing

---
+++ src/app/components/pages/yanatile/himno/himno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-himno',
+  standalone: true,
+  imports: [],
+  templateUrl: './himno.component.html',
+  styleUrl: './himno.component.css'
+})
+export class HimnoComponent {
+
+}
