06cd37705b37830bbaee573d3acbee7be9c8a3c1
Ronald Huahuatico Soria <69460625+Romehe369@users.noreply.github.com>
2024-08-23 14:08:31 -0500

    update news
    shared news components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./components/shared/header/header.component";
 import { FooterComponent } from "./components/shared/footer/footer.component";
 import { InicioComponent } from "./components/pages/inicio/inicio.component";
 import { MvisionComponent } from "./components/pages/municipalidad/mvision/mvision.component";
+import { NoticiasComponent } from "./components/pages/noticias/noticias.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, InicioComponent, MvisionComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, InicioComponent, MvisionComponent, NoticiasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
