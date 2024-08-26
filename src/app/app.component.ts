ea6ce6f757139ff2251033deac636170703c8687
Ronald Huahuatico Soria <69460625+Romehe369@users.noreply.github.com>
2024-08-26 12:44:31 -0500

    update caracteristicas
    update other componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { FooterComponent } from "./components/shared/footer/footer.component";
 import { InicioComponent } from "./components/pages/inicio/inicio.component";
 import { MvisionComponent } from "./components/pages/municipalidad/mvision/mvision.component";
 import { NoticiasComponent } from "./components/pages/noticias/noticias.component";
+import { HistoriaComponent } from "./components/pages/yanatile/historia/historia.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, InicioComponent, MvisionComponent, NoticiasComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, InicioComponent, MvisionComponent, NoticiasComponent, HistoriaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
