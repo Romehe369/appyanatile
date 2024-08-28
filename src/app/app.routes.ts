e5b8c7d0c7a19af630a677447a8bda2f9e60fd4d
Ronald Huahuatico Soria <69460625+Romehe369@users.noreply.github.com>
2024-08-27 21:32:11 -0500

    Adding routing
    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/pages/inicio/inicio.component';
+import { YanatileComponent } from './components/pages/yanatile/yanatile.component';
+import { HimnoComponent } from './components/pages/yanatile/himno/himno.component';
+import { FestividadesComponent } from './components/pages/yanatile/festividades/festividades.component';
+import { AtractivosComponent } from './components/pages/yanatile/atractivos/atractivos.component';
+import { HistoriaComponent } from './components/pages/yanatile/historia/historia.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+{ path: 'inicio', component: InicioComponent},
+{ path: 'yanatile', component: YanatileComponent},
+{ path: 'himno', component: HimnoComponent },
+{ path: 'festividades', component: FestividadesComponent },
+{ path: 'atrativos', component: AtractivosComponent },
+{ path: 'historia', component: HistoriaComponent },
+{ path: '**', redirectTo: 'inicio', pathMatch: 'full'}
+];
