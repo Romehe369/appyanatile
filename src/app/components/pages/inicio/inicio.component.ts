6089963bdd52d87866a950faed2bb885e9b7ada3
Ronald Huahuatico Soria <69460625+Romehe369@users.noreply.github.com>
2024-09-03 17:30:08 -0500

    add new activities
    any

--- src/app/components/pages/inicio/inicio.component.ts
+++ src/app/components/pages/inicio/inicio.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RegidoresComponent } from "../municipalidad/regidores/regidores.component";
+import { OrdeanzamdyComponent } from '../transparencia/ordeanzamdy/ordeanzamdy.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RegidoresComponent],
+  imports: [RegidoresComponent,OrdeanzamdyComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  
 
 }
--- src/app/components/pages/transparencia/ordeanzamdy/ordeanzamdy.component.ts
+++ src/app/components/pages/transparencia/ordeanzamdy/ordeanzamdy.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ordeanzamdy',
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './ordeanzamdy.component.html',
   styleUrl: './ordeanzamdy.component.css'
 })
-export class OrdeanzamdyComponent {
+export class OrdeanzamdyComponent implements OnInit {
+
+  ngOnInit() {
+    //updateTime();
+  }
+  /*function updateTime(): void {
+    const now: Date = new Date();
+    const days: string[] = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+    const months: string[] = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+    
+    const dayName: string = days[now.getDay()];
+    const day: number = now.getDate();
+    const month: string = months[now.getMonth()];
+    const year: number = now.getFullYear();
+
+    const hours: string = String(now.getHours()).padStart(2, '0');
+    const minutes: string = String(now.getMinutes()).padStart(2, '0');
+    const seconds: string = String(now.getSeconds()).padStart(2, '0');
+
+    const dateString: string = `${dayName}, ${day} de ${month} de ${year}`;
+    const timeString: string = `${hours}:${minutes}:${seconds}`;
+
+    const dateElement = document.getElementById('date');
+    const timeElement = document.getElementById('time');
+
+    if (dateElement && timeElement) {
+        dateElement.textContent = dateString;
+        timeElement.textContent = timeString;
+    }
+}
+
+setInterval(updateTime, 1000);
+updateTime(); // Inicializar el reloj inmediatamente*/
 
 }
