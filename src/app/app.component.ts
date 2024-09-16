87a05677ff16ffc4b09d623cf5c858325545181d
Ronald Huahuatico Soria <69460625+Romehe369@users.noreply.github.com>
2024-09-16 05:40:59 -0500

    update styles
    new action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ import { HimnoComponent } from "./components/pages/yanatile/himno/himno.componen
 import { ObrasComponent } from "./components/pages/obras/obras.component";
 import { ServiciosComponent } from "./components/pages/servicios/servicios.component";
 import { AtractivosComponent } from "./components/pages/yanatile/atractivos/atractivos.component";
+import { OrdeanzamdyComponent } from "./components/pages/transparencia/ordeanzamdy/ordeanzamdy.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, InicioComponent, MvisionComponent, NoticiasComponent, HistoriaComponent, HimnoComponent, ObrasComponent, ServiciosComponent, AtractivosComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, InicioComponent, MvisionComponent, NoticiasComponent, HistoriaComponent, HimnoComponent, ObrasComponent, ServiciosComponent, AtractivosComponent, OrdeanzamdyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
